8f0daed08569be847766c280ec4938c2b94075e9
KawenaG <45134325+KawenaG@users.noreply.github.com>
2019-12-07 21:04:00 -0800

    Global, Local and debug variables

--- JS/Basic_JavaScript_6.js
+++ JS/Basic_JavaScript_6.js
@@ -1,9 +1,37 @@
-function Car_Function() {
-    document.getElementById("Nested_Function").innerHTML = Count();
-    function Count() {
-        var Starting_point = 9;
-        function Plus_one() {Starting_point += 1;}
-        Plus_one() ;
-        return Starting_point;
-    }
+var X = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
 }
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+
+
+
+
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    var X =
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+
+
+
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_3();
---
+++ JS/Basic_JavaScript_7.js
@@ -0,0 +1,9 @@
+function Car_Function() {
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one() ;
+        return Starting_point;
+    }
+}
