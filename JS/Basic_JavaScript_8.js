b6692573456f42122fcbebd0efeeeb69be57ddbe
KawenaG <45134325+KawenaG@users.noreply.github.com>
2019-12-11 15:15:23 -0800

    toFixed and valueOf functions

--- JS/Basic_JavaScript_8.js
+++ JS/Basic_JavaScript_8.js
@@ -1,8 +1,5 @@
-function full_Sentence() {
-    var part_1 = "var one ";
-    var part_2 = "combined with var two ";
-    var part_3 = "and three ";
-    var part_4 = "can be joined to concatenate a sentence.";
-    var whole_sentence = part_1.concat(part_2, part_3, part_4);
-    document.getElementById("Concatenate").innerHTML = whole_sentence;
+function Value_of() {
+    var str = "true value";
+    var res = str.valueOf();
+    document.getElementById("test").innerHTML = res;
 }
